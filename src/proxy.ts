4b42d20fefa48b5821ccb1b8b47cbd5e94b0f98a
Habiba Hassan <habibahassan232001@gmail.com>
2026-05-03 10:36:35 +0200

    second commit

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,11 @@
+import { cookies } from 'next/headers';
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(req:NextRequest){
 
-
-    const token =await getToken({req})
+    const cookiesName = process.env.NODE_ENV =='production'?'__Secure-next-auth.session-token':'next-auth.session-token'
+    const token =await getToken({req:request,secret:process.env.NEXTAUTH_SECRET,cookieName})
     console.log("token");
     
     if(!!token)
--- src/utilities/getTokenFun.ts
+++ src/utilities/getTokenFun.ts
@@ -7,7 +7,7 @@ import { cookies } from "next/headers";
 
 export async function getTokenFn():Promise<string>{
     const cookie =await cookies()
-    const nextAuthToken = cookie.get('next-auth.session-token')?.value
+    const nextAuthToken = cookie.get('next-auth.session-token')?.value||cookie.get('__Secure-next-auth.session-token')?.value
     const decodeCookie =await decode({secret:process.env.NEXTAUTH_SECRET!,token:nextAuthToken})
 
     // console.log("decodeCookie",decodeCookie);
